126337aca07afaf0a9c87f345669394c0f194ded
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2023-02-11 21:09:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import math
 a = input("A: ")
 b = input("B: ")
 c = input("C: ")
-
 delta = b ** 2 - (4 * a * c)
 primary = -b + (math.sqrt(delta))
 secondary = -b - (math.sqrt(delta))
 result1 = primary / (2 * a)
 result2 = secondary / (2 * a)
-
 print(f"x = {result1}")
 print(f"x2 = {result2}")
